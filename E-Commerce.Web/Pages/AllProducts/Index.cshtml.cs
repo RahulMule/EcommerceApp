a5015b7405230976cc08447d989d2921686ee782
Rahul Popat Mule <rahulmuleofficial@gmail.com>
2024-03-02 17:55:36 +0530

    Update Index.cshtml.cs

--- E-Commerce.Web/Pages/AllProducts/Index.cshtml.cs
+++ E-Commerce.Web/Pages/AllProducts/Index.cshtml.cs
@@ -3,23 +3,24 @@ using E_Commerce.Web.Models;
 using E_Commerce.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace E_Commerce.Web.Pages.Product
 {
     public class IndexModel : PageModel
     {
-		private readonly IProductService _productService;
-		
-		public IEnumerable<E_Commerce.Web.Models.Product> products { get; set; }
+        private readonly IProductService _productService;
+        
+        public IEnumerable<E_Commerce.Web.Models.Product> products { get; set; }
 
         public IndexModel(IProductService productService)
         {
-			_productService = productService;
+            _productService = productService;
         }
 
         public void OnGet()
         {
             products = (IEnumerable<Models.Product>)_productService.GetAllProduct();
-          
-        }	
+        }
+    }
 }
